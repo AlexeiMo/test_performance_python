986abc6bb4e778a28a548cae0f3bea37891e8fd8
RuslanGitelman <47743960+RuslanGitelman@users.noreply.github.com>
2021-03-30 18:01:24 +0300

    - remove debug tools

--- helpers/assertion_helper.py
+++ helpers/assertion_helper.py
@@ -1,3 +1,5 @@
 def assert_status_code(response, expected_status_code=200):
     if response.status_code != expected_status_code:
         response.failure(f"Status code is not correct ({response.status_code})")
+    else:
+        response.success()
